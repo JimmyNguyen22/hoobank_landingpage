df830be55199f1463966ca040d8600532cfbf3ae
jimmy <vphu456@gmail.com>
2022-10-15 15:16:01 +0700

    business

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,9 +47,9 @@ const Hero = () => (
       <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"></div>{" "}
       <div className="absolute z-[1] w-[80%] h-[80%] bottom-40 rounded-full white__gradient"></div>{" "}
       <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient"></div>
-      <div className={`ss:hidden ${styles.flexCenter}`}>
-        <GetStarted></GetStarted>
-      </div>
+    </div>
+    <div className={`ss:hidden ${styles.flexCenter}`}>
+      <GetStarted></GetStarted>
     </div>
   </section>
 );
